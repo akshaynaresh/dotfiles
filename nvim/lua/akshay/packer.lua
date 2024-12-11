11dc626749b934e766a87b99117e3de1ac03fbe8
akshaynaresh <akshaydwivedi0606@gmail.com>
2024-12-11 23:30:05 +0530

    making changes in dotfiles

--- nvim/lua/akshay/packer.lua
+++ nvim/lua/akshay/packer.lua
@@ -4,47 +4,52 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use { 'neoclide/coc.nvim', branch='release' }
 
-	require('packer').startup(function(use)
-		use({ 'rose-pine/neovim', as = 'rose-pine' })
-	end)
-
-	use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use 'nvim-treesitter/playground'
-	use 'theprimeagen/harpoon'
-	use 'mbbill/undotree'
+    require('packer').startup(function(use)
+        use({ 'rose-pine/neovim', as = 'rose-pine' })
+    end)
     use 'joshdick/onedark.vim'
-	use 'tpope/vim-fugitive'
+
     use 'tpope/vim-commentary'
     use 'tpope/vim-surround'
     use 'vim-airline/vim-airline'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},     -- Required
-			{'hrsh7th/cmp-nvim-lsp'}, -- Required
-			{'L3MON4D3/LuaSnip'},     -- Required
-		}
-	}
-
-    use { 'neoclide/coc.nvim', branch='release' }
     use 'lervag/vimtex'
     use 'xuhdev/vim-latex-live-preview'
     use 'github/copilot.vim'
+
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use 'nvim-treesitter/playground'
+
+    use 'theprimeagen/harpoon'
+    use 'nvim-lua/plenary.nvim'
+    use 'mbbill/undotree'
+
+    use 'tpope/vim-fugitive'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            {'neovim/nvim-lspconfig'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+
 
 end)
